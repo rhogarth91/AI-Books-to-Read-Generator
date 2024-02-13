e3eed0e9e0472d088bfaaa7e3187dedc9e58f3fc
rhogarth91 <148714164+rhogarth91@users.noreply.github.com>
2024-02-13 17:08:27 +0000

    Amends to HTML, CSS and JS (list below)
    HTML form altered,
    HTML IDs added,
    CSS improved following HTML amends,
    Axios API and Typewriter Plugin inserted,
    Function for AI started

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generateBookSuggestion() {
+  let apiKey = "74a685e33bd3f486faa1o3tac33e021d";
+  let context = "Please be polite and provide a short answer.";
+  let prompt =
+    "Please tell me what books I should read based on the key wrods entered. Can these suggestions be limited to three books. Please provide the book title and authors name";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+}
 
+let waitingMessage = document.querySelector("#book-suggestion");
+waitingMessage.innerHTML =
+  "Please wait bookworm...... generating some suggestions for you now!";
+
+let bookFormElement = document.querySelector("#book-suggestion-form");
+bookFormElement.addEventListener("submit", generateBookSuggestion);
