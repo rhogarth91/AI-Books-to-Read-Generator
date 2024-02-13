a00582ac6ed787b62e817943607be0ce6966a0a7
rhogarth91 <148714164+rhogarth91@users.noreply.github.com>
2024-02-13 17:48:21 +0000

    Function added/completed, HTML improved
    -Function now generates, using an AI API, 3 book suggestions based on the users keywords input in the form.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-function generateBookSuggestion() {
-  let apiKey = "74a685e33bd3f486faa1o3tac33e021d";
-  let context = "Please be polite and provide a short answer.";
-  let prompt =
-    "Please tell me what books I should read based on the key wrods entered. Can these suggestions be limited to three books. Please provide the book title and authors name";
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+function showBooks(response) {
+  new Typewriter("#book-suggestion", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: "",
+    delay: 50,
+  });
 }
 
-let waitingMessage = document.querySelector("#book-suggestion");
-waitingMessage.innerHTML =
-  "Please wait bookworm...... generating some suggestions for you now!";
+function generateBookSuggestion(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-input");
+  let apiKey = "74a685e33bd3f486faa1o3tac33e021d";
+  let context =
+    "Please tell me what books I should read as you are a book expert and love books. Can you generate three book suggestions following the user instructions. Please provide the book title and authors name for each of the three suggestions";
+  let prompt = `User Instructions: Please generate a book suggestion based on the ${instructionsInput.value}. Please be polite and provide a short answer.`;
+
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let waitingMessage = document.querySelector("#book-suggestion");
+  waitingMessage.innerHTML =
+    "Please wait bookworm...... generating some suggestions for you now!";
+
+  axios.get(apiUrl).then(showBooks);
+}
 
 let bookFormElement = document.querySelector("#book-suggestion-form");
 bookFormElement.addEventListener("submit", generateBookSuggestion);
