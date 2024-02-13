3de8562bad3fded2c012cd37c95057736857758a
rhogarth91 <148714164+rhogarth91@users.noreply.github.com>
2024-02-13 18:17:00 +0000

    AI context improved, CSS improved and HTML form placeholder amends

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ function generateBookSuggestion(event) {
   let instructionsInput = document.querySelector("#user-input");
   let apiKey = "74a685e33bd3f486faa1o3tac33e021d";
   let context =
-    "Please tell me what books I should read as you are a book expert and love books. Can you generate three book suggestions following the user instructions. Please provide the book title and authors name for each of the three suggestions";
-  let prompt = `User Instructions: Please generate a book suggestion based on the ${instructionsInput.value}. Please be polite and provide a short answer.`;
+    "Please tell me what books I should read as you are a book expert, you love books and are very well read. Can you generate three book suggestions using the user instructions. Please provide ONLY the book title and authors name for each of the three suggestions. Can the suggestions be in a list format, for example: 1. Book, 2. Book, 3 Book. The answer must be presented in basic HTML and separate each line with a <br />";
+  let prompt = `User Instructions: Please generate a book suggestion based on the ${instructionsInput.value}.`;
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
